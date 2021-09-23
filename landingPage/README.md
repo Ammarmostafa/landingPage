d5d1b83f7eb173afe648dd1b93bb49d4cf67fe35
Ammarmostafa <36388453+Ammarmostafa@users.noreply.github.com>
2021-09-23 04:09:13 +0200

    Submiting the project

---
+++ landingPage/README.md
@@ -0,0 +1,16 @@
+# Landing Page Project
+
+This is my first project, and I hope to be successful in it.
+
+I have added the sections via JavaScript.
+
+I created a function to create a link for each section.
+
+Modified the active class properties for both section and link.
+
+I made a button to create additional sections.
+
+I made an icon to go to the top of the page smoothly and appear after scaling to 850 pixels.
+
+I made the link header disappear after 10 seconds and reappear with just a scroll.
+
---
+++ landingPage/js/app.js
@@ -0,0 +1,93 @@
+/**
+ * creating sections dynamically
+ * by using createSection function and add them to the main tag
+ * ES6
+ */
+// counter to specify attributes ,id and number of section
+let counter = 0;
+const createSection = () => {
+  counter++;
+  const content = `<section id="section${counter}" data-nav="Section ${counter}">
+    <div class="landing__container">
+    <h2>Section ${counter}</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
+    
+    <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+    </div>
+    </section>`;
+  document.querySelector("main").insertAdjacentHTML("beforeend", content);
+};
+/**
+ * make list items equal to the number of sections by iterate on them
+ * I removed all items to avoid the duplicating while adding new section
+ */
+const navBar = document.getElementById("navbar__list");
+const createNavItems = () => {
+  navBar.innerHTML = "";
+  document.querySelectorAll("section").forEach((section) => {
+    const listItem = `<li><a href="#${section.id}" data-nav="${section.id}" class="menu__link">${section.dataset.nav}</a></li>`;
+    navBar.insertAdjacentHTML("beforeend", listItem);
+  });
+};
+
+
+
+// using Element.getBoundingClientRect()
+window.onscroll = function() {
+	document.querySelectorAll("section").forEach(function(active) {
+    let activeLink = navBar.querySelector(`[data-nav=${active.id}]`);
+	if(active.getBoundingClientRect().top >= -400 && active.getBoundingClientRect().top <= 150){
+
+    active.classList.add("your-active-class");
+    activeLink.classList.add("active-link");
+
+    }
+    else{
+         active.classList.remove("your-active-class");
+         activeLink.classList.remove("active-link");
+    }
+	});
+}
+
+/**
+ * create five-section dynamically by javascript instead of HTML
+ * create them links
+ */
+for (let i = 1; i < 6; i++) createSection();
+createNavItems();
+
+
+// creating more sections by click on the button
+document.getElementById("btn").addEventListener("click", () => {
+  createSection();
+  createNavItems();
+  
+});
+// save the icon used to go to the top and the header in variables
+const toTop = document.getElementById("to-top");
+const header = document.querySelector(".page__header");
+
+// Clicking on the icon the document will scroll to the top smoothly
+toTop.addEventListener("click", () => {
+  document.documentElement.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+ //disappear the header after 10 seconds and appear again when scrolling down.
+let isScrolling;
+document.onscroll = () => {
+  header.style.display = "block"
+  clearTimeout(isScrolling)
+   isScrolling = setTimeout(() => {
+    header.style.display = "none";
+  }, 10000);
+
+  //appearing the icon(to-top) after 850px to down
+  window.scrollY > 850
+    ? (toTop.style.display = "block")
+    : (toTop.style.display = "none");
+};
+
+
+
+
+
